299928f55110fa2647c76fe20b29c6331821126b
Kozachok_Maria <143996292+humyoo@users.noreply.github.com>
2023-11-22 00:46:32 +0200

    Add files via upload

---
+++ array3.cpp
@@ -0,0 +1,56 @@
+﻿// array3.cpp
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+void fillArray(int arr[], int size) {
+    srand(time(nullptr));
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 100;
+    }
+}
+
+void printArray(int arr[], int size) {
+
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void swapMinMax(int arr[], int size) {
+
+    int minIndex = 0, maxIndex = 0;
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[maxIndex];
+    arr[maxIndex] = temp;
+}
+
+int main() {
+    const int size = 10; 
+    int myArray[size];
+
+    fillArray(myArray, size);
+
+    std::cout << "Started array: ";
+    printArray(myArray, size);
+
+    swapMinMax(myArray, size);
+
+    std::cout << "Array after replacement: ";
+    printArray(myArray, size);
+
+    return 0;
+}
+
---
+++ array4.cpp
@@ -0,0 +1,45 @@
+﻿// array4.cpp 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int A;
+    cout << "Enter number A: ";
+    cin >> A;
+
+
+    int n;
+    cout << "Enter size Array: ";
+    cin >> n;
+
+
+    int* arr = new int[n];
+    cout << "Enter element Array:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > A) {
+            count++;
+        }
+    }
+
+
+    cout << "The amount of elements is greater than " << A << ": " << count << endl;
+
+
+    delete[] arr;
+    cout << "Press Enter to exit...";
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
+
